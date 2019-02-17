1ae268c9823d9f2f5ed77aad0ffa966f6c793374
Matthew Holman <mateoholman@gmail.com>
2019-02-17 14:29:51 -0600

    Create LocalDT component

---
+++ src/components/LocalDT/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class LocalDT extends React.Component {
+    state = {
+        currentDateTime: new Date()
+    }
+
+    updateDateTime = () => setInterval(() => {
+        this.setState({ currentDateTime: new Date() })
+    }, 1000);
+
+    componentDidMount() {
+        this.updateDateTime();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.currentDateTime}</h1>
+            </div>
+        );
+    }
+}
+
+export default LocalDT;
