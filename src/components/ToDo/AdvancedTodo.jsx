522f56565ad2ff903239a63effed2c3a9abd7669
Matthew Holman <mateoholman@gmail.com>
2019-03-30 15:51:24 -0500

    Convert class to functional component and use effects

--- src/components/ToDo/AdvancedTodo.jsx
+++ src/components/ToDo/AdvancedTodo.jsx
@@ -1,93 +1,74 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NewTodo from "./NewTodo";
 import TodoItem from "./TodoItem";
 import { Container, List } from "./Styled";
 
-export default class AdvancedTodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      newTodo: "",
-    };
-    this.handleNewChange = this.handleNewChange.bind(this);
-    this.handleNewSubmit = this.handleNewSubmit.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleCompletedToggle = this.handleCompletedToggle.bind(this);
+export default function AdvancedTodoList() {
+  const [newTodo, updateNewTodo] = useState('');
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos")) || [];
+  const [todos, updateTodos] = useState(initialTodos);
+
+  // update(todos) {
+  //   const inCompleteTodos = todos.reduce(
+  //     (memo, todo) => (!todo.completed ? memo + 1 : memo),
+  //     0
+  //   );
+  //   document.title = inCompleteTodos ? `Todos (${inCompleteTodos})` : "Todos";
+  //   window.localStorage.setItem("todos", JSON.stringify(todos));
+  // }
+  // componentDidMount() {
+  //   const todos = JSON.parse(window.localStorage.getItem("todos") || "[]");
+  //   this.update(todos);
+  //   this.setState({ todos });
+  // }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.todos !== this.state.todos) {
+  //     this.update(this.state.todos);
+  //   }
+  // }
+
+  const handleNewChange = (e) => {
+    updateNewTodo(e.target.value);
   }
-  update(todos) {
-    const inCompleteTodos = todos.reduce(
-      (memo, todo) => (!todo.completed ? memo + 1 : memo),
-      0
-    );
-    document.title = inCompleteTodos ? `Todos (${inCompleteTodos})` : "Todos";
-    window.localStorage.setItem("todos", JSON.stringify(todos));
-  }
-  componentDidMount() {
-    const todos = JSON.parse(window.localStorage.getItem("todos") || "[]");
-    this.update(todos);
-    this.setState({ todos });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.todos !== this.state.todos) {
-      this.update(this.state.todos);
-    }
-  }
-  handleNewChange(e) {
-    this.setState({
-      newTodo: e.target.value
-    });
-  }
-  handleNewSubmit(e) {
+
+  const handleNewSubmit = (e) => {
     e.preventDefault();
-    this.setState(prevState => {
-      return {
-        todos: [
-          ...prevState.todos,
-          { id: Date.now(), text: prevState.newTodo, completed: false }
-        ],
-        newTodo: ""
-      };
-    });
+    updateTodos(prevTodos => [
+      ...prevTodos,
+      { id: Date.now(), text: newTodo, completed: false }
+    ]);
+    updateNewTodo('');
   }
-  handleDelete(id, e) {
-    this.setState(prevState => {
-      return {
-        todos: prevState.todos.filter(todo => todo.id !== id)
-      };
-    });
+
+  const handleDelete = (id, e) => {
+    updateTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
   }
-  handleCompletedToggle(id, e) {
-    this.setState(prevState => {
-      return {
-        todos: prevState.todos.map(todo =>
-          todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        )
-      };
-    });
+
+  const handleCompletedToggle = (id, e) => {
+    updateTodos(prevTodos => prevTodos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
   }
-  render() {
-    const { newTodo, todos } = this.state;
-    return (
-      <Container todos={todos}>
-        <NewTodo
-          onSubmit={this.handleNewSubmit}
-          value={newTodo}
-          onChange={this.handleNewChange}
-        />
-        {!!todos.length && (
-          <List>
-            {todos.map(todo => (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                onChange={this.handleCompletedToggle}
-                onDelete={this.handleDelete}
-              />
-            ))}
-          </List>
-        )}
-      </Container>
-    );
-  }
+
+  return (
+    <Container todos={todos}>
+      <NewTodo
+        onSubmit={handleNewSubmit}
+        value={newTodo}
+        onChange={handleNewChange}
+      />
+      {!!todos.length && (
+        <List>
+          {todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onChange={handleCompletedToggle}
+              onDelete={handleDelete}
+            />
+          ))}
+        </List>
+      )}
+    </Container>
+  );
 }
