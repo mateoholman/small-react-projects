b65ef639bd26770fb8d54548806ad0c0346288f5
Matthew Holman <mateoholman@gmail.com>
2019-04-19 18:24:50 -0500

    Use community title hook

--- src/components/ToDo/AdvancedTodo.jsx
+++ src/components/ToDo/AdvancedTodo.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
-import NewTodo from "./NewTodo";
+import useTitle from "react-use/lib/useTitle";
 import TodoItem from "./TodoItem";
 import { Container, List } from "./Styled";
+import NewTodo from './NewTodo';
 
 export default function AdvancedTodoList() {
   const [newTodo, updateNewTodo] = useState('');
   const todoId = useRef(0);
   const initialTodos = () => {
     const valueFromStorage = JSON.parse(window.localStorage.getItem("todos")) || [];
-    todoId.current = valueFromStorage.reduce(
-      (memo, todo) => Math.max(memo, todo.id),
-      0
-    );
+    todoId.current = valueFromStorage.reduce((memo, todo) => Math.max(memo, todo.id), 0);
     return valueFromStorage;
   }
   const [todos, updateTodos] = useState(initialTodos);
@@ -19,13 +17,12 @@ export default function AdvancedTodoList() {
     window.localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  useEffect(() => {
-    const inCompleteTodos = todos.reduce(
-      (memo, todo) => (!todo.completed ? memo + 1 : memo),
-      0
-    );
-    document.title = inCompleteTodos ? `Todos (${inCompleteTodos})` : "Todos";
-  });
+  const inCompleteTodos = todos.reduce(
+    (memo, todo) => (!todo.completed ? memo + 1 : memo),
+    0
+  );
+  const title = inCompleteTodos ? `Todos (${inCompleteTodos})` : "Todos";
+  useTitle(title);
 
   const handleNewChange = (e) => {
     updateNewTodo(e.target.value);
